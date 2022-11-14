cb36b7647871b4398ccd9471fcec93d646fa1d2e
sevenChrisK <88460982+sevenChrisK@users.noreply.github.com>
2022-11-14 22:12:14 +0000

    Create data_access.py
    Script with helper functions wrapping access to shelf used by project to store data.

---
+++ data_access.py
@@ -0,0 +1,34 @@
+"""Minimal wrapper for shelve to cover my most commonly used operations"""
+import shelve
+
+
+def get_db_keys() -> list[str]:
+    """Helper function to get list of db keys"""
+    with shelve.open('data/db/data_store') as db:
+        keys_list = list(db.keys())
+
+    return keys_list
+
+
+def load_from_db(key_string: str) -> object:
+    """Helper function to load data from db using a key string"""
+    with shelve.open('data/db/data_store') as db:
+        loaded_item = db[key_string]
+
+    return loaded_item
+
+
+def save_to_db(key_string: str, data_to_save: object) -> None:
+    """Helper function to save data to db using a key string"""
+    with shelve.open('data/db/data_store') as db:
+        db[key_string] = data_to_save
+
+    return
+
+
+def report_shelf_contents() -> None:
+    """Helper function to report top level contents of the shelf"""
+    for key in get_db_keys():
+        print(f"{key}: ")
+        print(f"{len(load_from_db(key))} items stored.")
+        print("\n")
