7bf34e9d5b3161404ca606b6810c9868065850b1
sevenChrisK <88460982+sevenChrisK@users.noreply.github.com>
2022-11-16 22:02:31 +0000

    Write function to strip stopwords

--- pre_processing.py
+++ pre_processing.py
@@ -1,4 +1,5 @@
 from nltk import PunktSentenceTokenizer
+from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 
 sentence_tokenizer = PunktSentenceTokenizer()
@@ -32,3 +33,34 @@ def tokenize_articles() -> None:
                data_to_save=article_list)
 
     return None
+
+
+def strip_stopwords() -> None:
+    """Strips stopwords from the article tokens"""
+    article_list = load_from_db(db_key_string='filtered_articles')
+    stop_words = set(stopwords.words('english'))
+
+    for article in article_list:
+        # Strip stopwords from word tokens
+        article['words'] = [word for word in article['words'] if word not in stop_words]
+
+        # Strip stopwords from sentence tokens
+        article['sentences'] = [remove_sentence_stopwords(sentence) for sentence in article['sentences']]
+
+    # Save the updated list for later use
+    save_to_db(db_key_string='filtered_articles',
+               data_to_save=article_list)
+
+    return None
+
+
+def remove_sentence_stopwords(input_sentence: str) -> str:
+    """Take a sentence, remove stopwords, and return a sentence"""
+    stop_words = set(stopwords.words('english'))
+
+    word_list = word_tokenize(input_sentence)
+    word_list = [word for word in word_list if word not in stop_words]
+    returned_sentence = " ".join(word_list)
+
+    return returned_sentence
+
